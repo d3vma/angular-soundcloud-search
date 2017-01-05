7a674d10dbee3884121ee742c1ac990d3bf663af
Adel <mh4md.a@gmail.com>
2017-01-05 20:31:09 +0200

    Add filter to retrieve duration in minutes

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 var app = angular.module("soundcloudSearch", []);
+
+// Filter to convert milliseconds to minutes 
+app.filter('durationMins', function() {
+    return function (s) {
+        var ms = s % 1000;
+        s = (s - ms) / 1000;
+        var secs = s % 60;
+        s = (s - secs) / 60;
+        var mins = s % 60;
+        return mins + ':' + secs;        
+    };
+});
